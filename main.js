30a926612eed7f364740d767af0a92fde5c37d5b
pooja14magar <magarpooja596@gmail.com>
2024-11-28 18:34:05 +0530

    practice-DOM

---
+++ main.js
@@ -0,0 +1,20 @@
+//  let heading = document.getElementById("heading");
+//  console.dir(heading);
+
+// let headings= document.getElementsByClassName("heading");
+// console.dir(headings);
+// console.log(headings);
+
+// let parah= document.getElementsByTagName("p");
+// console.dir(parah);
+// console.log(parah);
+
+// let firstElement = document.querySelector(".heading");
+// console.dir(firstElement);
+
+
+// let allElement = document.querySelectorAll(".heading");
+// console.dir(allElement);
+
+let div = document.querySelector("div");
+console.dir(div);
